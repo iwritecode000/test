bc59432805b049c3d12b71aec37d19d5e1035bbf
Ritam <iwritecode000@gmail.com>
2022-11-01 11:34:51 +0530

    Feature & stats aded

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Service from '../components/Service'
 import Feature from '../components/Feature'
+import Stats from '../components/Stats'
 import Link from 'next/link'
 
 export default function Home() {
@@ -24,6 +25,7 @@ export default function Home() {
       <Feature/>
       <Blogcards />
       <Tourcards />
+      <Stats/>
     </>
   )
 }
